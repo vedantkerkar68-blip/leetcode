8cb28f7ec9da9f53a5d5255c6aca8728b679040d
vedantkerkar68-blip <vedantkerkar68@gmail.com>
2026-06-09 23:14:34 +0530

    Time: 2 ms (91.78%) | Memory: 47.7 MB (55.73%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int pos=0;
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[pos];
+                nums[pos]=temp;
+                pos++;
+            }
+        }
+       
+        
+    }
+}
