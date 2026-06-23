56bfec5721dc2261111ba9bc490761c6dcaa9170
vedantkerkar68-blip <vedantkerkar68@gmail.com>
2026-06-23 13:51:55 +0530

    Time: 1 ms (99.98%) | Memory: 42.6 MB (63.29%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+       long rev = 0;
+        
+        while(x != 0) {
+            int last = x % 10;
+            rev = (rev * 10) + last;
+
+            if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+                return 0;
+            }
+            x = x / 10;
+        }
+        
+        return (int) rev;
+
+        }
+    }
