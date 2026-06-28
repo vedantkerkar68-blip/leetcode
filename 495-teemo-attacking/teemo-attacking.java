549b40bc05116806625e19ab19b7a89d2f215cba
vedantkerkar68-blip <vedantkerkar68@gmail.com>
2026-06-28 21:23:06 +0530

    Time: 3 ms (87.82%) | Memory: 48.2 MB (57.86%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.java
+++ 495-teemo-attacking/teemo-attacking.java
@@ -1,15 +1,15 @@
 class Solution {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
      
-     int s=duration;
+     int total = duration;
      for(int i=1;i<timeSeries.length;i++){
         if(timeSeries[i]<(timeSeries[i-1]+duration)){
-            s+=(duration-(duration-(timeSeries[i]-timeSeries[i-1])));
+            total += timeSeries[i] - timeSeries[i - 1];
         }
         else{
-            s+=duration;
+            total+=duration;
         }
      } 
-     return s;
+     return total;
     }
 }
